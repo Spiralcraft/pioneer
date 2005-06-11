03da8cd445ed30f0bba773fcf34f8836d5fea8d3
mike <>
2005-06-11 02:42:42 +0000

    Source distribution, copyrights

--- source/spiralcraft/pioneer/data/text/MergeTemplate.java
+++ source/spiralcraft/pioneer/data/text/MergeTemplate.java
@@ -1,7 +1,18 @@
-/**
- * A text-merge template compiled into a form to facilitate generation
- *   from EntityLists.
- */
+//
+// Copyright (c) 1998,2005 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+
 package spiralcraft.pioneer.data.text;
 
 import com.spiralcraft.data.lang.ValueContext;
